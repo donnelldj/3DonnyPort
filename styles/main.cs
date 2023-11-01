78cd500c75f43e1a8b0d6f9d50646a4dc1cb9f8d
donnelldj <dperki01@nyit.edu>
2023-11-01 10:17:58 -0400

    First commit: File architecture

---
+++ styles/main.cs
@@ -0,0 +1,14 @@
+body {
+    font-family: Arial, sans-serif;
+    color: #fff;
+    background-color: #000;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+}
+
+nav, header, section, footer {
+    padding: 20px;
+}
+
+/* Add more specific styles for each section, elements, responsive design, etc. */
