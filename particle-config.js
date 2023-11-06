bb8941036cc8d717d7f37a1935317a0f60837165
donnelldj <dperki01@nyit.edu>
2023-11-06 09:22:27 -0500

    Cool 3D objects and threejs.backend-_Check

---
+++ particle-config.js
@@ -0,0 +1,57 @@
+
+    
+    // particles-config.js
+    particlesJS("particles-js", {
+    "particles": {
+        "number": {
+        "value": 50,
+        "density": {
+            "enable": true,
+            "value_area": 800
+        }
+        },
+        "color": {
+        "value": "#000000"
+        },
+        "shape": {
+        "type": "circle",
+        "stroke": {
+            "width": 0,
+            "color": "#000000"
+        }
+        },
+        "opacity": {
+        "value": 1,
+        "random": false
+        },
+        "size": {
+        "value": 3,
+        "random": true
+        },
+        "line_linked": {
+        "enable": false
+        },
+        "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+        "onhover": {
+            "enable": false
+        },
+        "onclick": {
+            "enable": false
+        },
+        "resize": true
+        }
+    },
+    "retina_detect": true
+    });
